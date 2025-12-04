b94c92d3005cac48981c0b1d5be1394f73561506
beans9926 <gulhanesarthak03@gmail.com>
2025-12-04 19:04:01 +0530

    add fullscreen toggle

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,8 @@
 #include <chrono>
 #include <ctime>
 
+int screenHeight = GetScreenHeight();
+int screenWidth = GetScreenWidth();
 int windowHeight = 100;
 int windowWidth = 500;
 Vector2 mousePos;
@@ -10,9 +12,9 @@ int radius = 0;
 std::time_t currentTime;
 
 int main (int argc, char *argv[]) {
-	InitWindow(windowWidth, windowHeight, "clock");
+	InitWindow(screenWidth, screenHeight, "clock");
 	SetTargetFPS(30);
-	ToggleBorderlessWindowed();
+	ToggleFullscreen();
 	while (!WindowShouldClose()) {
 		auto now = std::chrono::system_clock::now();
 		currentTime = std::chrono::system_clock::to_time_t(now);
@@ -23,9 +25,16 @@ int main (int argc, char *argv[]) {
 			mousePos = GetMousePosition();
 		}
 
+		if(IsMouseButtonPressed(MOUSE_BUTTON_MIDDLE)){
+			ToggleFullscreen();
+			SetWindowSize(windowWidth, windowHeight);
+		}
+
 		BeginDrawing();
 		ClearBackground(BLACK);
 		DrawText(std::ctime(&currentTime), 0, 0, 40, RAYWHITE);
+		DrawText("left click - ripple", 0, 60, 10, DARKGRAY);
+		DrawText("middle click - smol display", 0, 70, 10, DARKGRAY);
 		radius +=	2 ;
 		DrawCircleLines(mousePos.x, mousePos.y, radius, BLUE);
 
